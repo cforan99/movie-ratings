8a2535ae75245a5ef265e8020e9dbd59003d4600
Hackbright Student <info@hackbrightacademy.com>
2015-10-20 12:45:16 -0700

    Completed seed.py with load_... functions.  Works!

--- seed.py
+++ seed.py
@@ -1,12 +1,10 @@
 """Utility file to seed ratings database from MovieLens data in seed_data/"""
 
 
-from model import User
-# from model import Rating
-# from model import Movie
-
+from model import User, Rating, Movie
 from model import connect_to_db, db
 from server import app
+from datetime import datetime
 
 
 def load_users():
@@ -37,9 +35,58 @@ def load_users():
 def load_movies():
     """Load movies from u.item into database."""
 
+    print "Movies"
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    Movie.query.delete()
+
+    # Read u.item file and insert data
+    for row in open("seed_data/u.item"):
+        row = row.rstrip()
+        row = row.split("|")
+        
+        movie_id = row[0]
+        title = row[1]
+        string_date = row[2]
+        imdb_url = row[4]
+
+        title = title[:-7]
+        
+        try:
+            released_at = datetime.strptime(string_date, "%d-%b-%Y")
+        except ValueError:
+            released_at = None
+
+        movie = Movie(movie_id=movie_id,
+                      title=title,
+                      released_at=released_at,
+                      imdb_url=imdb_url)
+        
+        db.session.add(movie)
+
+    db.session.commit()
+
 
 def load_ratings():
     """Load ratings from u.data into database."""
+
+    print "Ratings"
+
+    Rating.query.delete()
+
+    for row in open("seed_data/u.data"):
+        row = row.rstrip()
+        user_id, movie_id, score, timestamp = row.split()
+
+        rating = Rating(movie_id=movie_id,
+                        user_id=user_id,
+                        score=score)
+
+        db.session.add(rating)
+
+    db.session.commit()
+
 
 
 if __name__ == "__main__":
