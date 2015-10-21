11826ff8137fddbc7b6c0822e90fdbe68c3a4e3f
Hackbright Student <info@hackbrightacademy.com>
2015-10-21 13:01:07 -0700

    Started flask app with templates and login route.

--- server.py
+++ server.py
@@ -2,10 +2,9 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-
-from model import connect_to_db, db
+from model import connect_to_db, db, User, Rating, Movie
 
 
 app = Flask(__name__)
@@ -22,7 +21,42 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+
+@app.route("/login-form")
+def login_form():
+    """Show login form"""
+
+    return render_template("login-form.html")
+
+
+@app.route("/login", methods=["POST"])
+def process_login():
+    """Find user in database by their email, check their password, and store
+    tuser id in session. Finally, redirect to homepage."""
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    # query database to find user with given email
+
+    # if email in db:
+    #     if password is correct:
+    #         store id in session
+    #         redirect to user page
+    #     else:
+    #         flash error message and redirect to login-form
+    # else:
+    #     flash error message and stay on login-form
+
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user-list.html", users=users)
 
 
 if __name__ == "__main__":
